89d3d36a670cd4f953a13b52ba57106f03b162b9
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-12-17 16:22:18 -0300

    ✨ feat(shared/errors): ErrZeroRowsAffected error created

--- internal/shared/errors/errors.go
+++ internal/shared/errors/errors.go
@@ -3,10 +3,11 @@ package errors
 import "errors"
 
 var (
-	ErrNotFound        = errors.New("resource not found")
-	ErrNothingToUpdate = errors.New("nothing to update")
-	ErrConflict        = errors.New("resource conflict")
-	ErrUnauthorized    = errors.New("unauthorized")
-	ErrForbidden       = errors.New("forbidden")
-	ErrValidation      = errors.New("validation error")
+	ErrNotFound         = errors.New("resource not found")
+	ErrNothingToUpdate  = errors.New("nothing to update")
+	ErrZeroRowsAffected = errors.New("zero rows affected")
+	ErrConflict         = errors.New("resource conflict")
+	ErrUnauthorized     = errors.New("unauthorized")
+	ErrForbidden        = errors.New("forbidden")
+	ErrValidation       = errors.New("validation error")
 )
