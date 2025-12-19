2382a4569fc66f1e597ec18b382fb3c06daad649
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-12-19 12:08:21 -0300

    ✨ feat(cmd/api/main.go): api setup updated

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,13 +6,18 @@ import (
 	"ticket-io/internal/config"
 	"ticket-io/internal/database"
 
+	authhandler "ticket-io/internal/auth/handler"
 	userhandler "ticket-io/internal/user/handler"
 
+	authservice "ticket-io/internal/auth/service"
 	statusservice "ticket-io/internal/user/service/status"
 	userservice "ticket-io/internal/user/service/user"
 
+	authrepository "ticket-io/internal/auth/repository"
 	statusrepository "ticket-io/internal/user/repository/status"
 	userrepository "ticket-io/internal/user/repository/user"
+
+	authmiddleware "ticket-io/internal/auth/middleware"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -29,18 +34,38 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
-	api := r.Group("/api/v1")
+	apiV1Group := r.Group("/api/v1")
 
 	// repositories
 	userRepo := userrepository.New(db)
 	statusRepo := statusrepository.New(db)
+	authRepo := authrepository.New(db)
 
 	// services
 	statusService := statusservice.New(statusRepo)
 	userService := userservice.New(userRepo, statusService)
+	authService := authservice.New(authRepo, cfg.JWTSecret)
 
-	// handlers & routes
-	userhandler.RegisterRoutes(api, userService)
+	// handlers
+	authHandler := authhandler.New(authService)
+	userHandler := userhandler.New(userService)
+
+	// middlewares
+	jwtMiddleware := authmiddleware.JWTAuthentication(cfg.JWTSecret)
+
+	// routes
+
+	// auth (public/no middleware)
+	auth := apiV1Group.Group("/auth")
+	{
+		auth.POST("/register", authHandler.RegisterUser)
+		auth.POST("/login", authHandler.LoginUser)
+	}
+
+	// users (protected)
+	userGroup := apiV1Group.Group("/users")
+	userGroup.Use(jwtMiddleware)
+	userhandler.RegisterRoutes(userGroup, userHandler)
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "Ok"})
